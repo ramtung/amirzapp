b9dbaefb89055a31ee7ca64c4a8fb9146ec38d22
ramtung <ramtung@gmail.com>
2025-01-13 17:46:48 +0330

    Scramble the letters automatically. Change the language to English.

--- phase-1/amirzapp-1.cpp
+++ phase-1/amirzapp-1.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
+string scramble(string s) {
+    for (int i = 0; i < s.length(); i++) {
+        int j = rand() % s.length();
+        swap(s[i], s[j]);
+    }
+    return s;
+}
+
+void print_scrambled(string s) {
+    string scrambled = scramble(s);
+    for (int i = 0; i < scrambled.length(); i++) {
+        cout << scrambled[i] << ' ';
+    }
+    cout << endl;
+}
+
 int main() {
-    const string target = "سازمان";
-    cout << "حروف کلمه: ا س م ن ا ز" << endl;
-    cout << "لطفاً کلمه‌ مورد نظر را وارد کنید. برای خروج از برنامه کنترل-دی را فشار دهید." << endl;
+    srand(time(0));
+
+    const string target = "prince";
+    cout << "The letters are: ";
+    print_scrambled(target);
+
+    cout << "Please enter the word. Press Ctrl-D to exit." << endl;
 
     string input;
     while (cin >> input) {
         if (input == target) {
-            cout << "تبریک! شما کلمه را پیدا کردید." << endl;
+            cout << "Congratulations! You found the target word!" << endl;
             break;
         } else {
-            cout << "کلمه‌ی وارد شده اشتباه است. لطفاً دوباره تلاش کنید." << endl;
+            cout << "Sorry, wrong word! Please try again!" << endl;
         }
     }
 }
